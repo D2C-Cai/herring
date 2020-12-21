932307af623240ece843c5e730a17588c2681969
Jackey <709931138@qq.com>
2020-12-21 18:54:23 +0800

    初始提交项目结构

---
+++ herring-services/herring-product/herring-product-service/src/main/java/com/herring/ProductServiceApplication.java
@@ -0,0 +1,31 @@
+package com.herring;/*
+@(#)herring   2020-12-21
+ 
+Copyright (c) 2011-2020 杭州湖畔网络技术有限公司 
+保留所有权利 
+本软件为杭州湖畔网络技术有限公司所有及包含机密信息，须遵守其相关许可证条款进行使用。
+Copyright (c) 2011-2020 HUPUN Network Technology CO.,LTD.
+All rights reserved.
+This software is the confidential and proprietary information of HUPUN
+Network Technology CO.,LTD("Confidential Information").  You shall not
+disclose such Confidential Information and shall use it only in
+accordance with the terms of the license agreement you entered into with HUPUN.
+Website：http://www.hupun.com
+*/
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @author: Jackey 2020/12/21
+ */
+@EnableDiscoveryClient
+@SpringBootApplication
+public class ProductServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProductServiceApplication.class, args);
+    }
+
+}
